73251dea656b12d82513e55010c5957af0604023
Dan <daniel.smirnov17@gmail.com>
2024-01-29 20:35:20 +0000

    added create & edit tasks

--- src/pages/tasks/list.tsx
+++ src/pages/tasks/list.tsx
@@ -7,10 +7,12 @@ import {
 import { ProjectCard, ProjectCardMemo } from "@/components/tasks/kanban/card";
 import { KanbanColumn } from "@/components/tasks/kanban/column";
 import { KanbanItem } from "@/components/tasks/kanban/item";
+import { UPDATE_TASK_STAGE_MUTATION } from "@/graphql/mutations";
 import { TASKS_QUERY, TASK_STAGES_QUERY } from "@/graphql/queries";
-import { TaskStage } from "@/graphql/schema.types";
+import { Task, TaskStage, TaskUpdateInput } from "@/graphql/schema.types";
 import { TasksQuery } from "@/graphql/types";
-import { useList, useNavigation } from "@refinedev/core";
+import { DragEndEvent } from "@dnd-kit/core";
+import { HttpError, useList, useNavigation, useUpdate } from "@refinedev/core";
 import { GetFieldsFromList } from "@refinedev/nestjs-query";
 import React from "react";
 
@@ -78,6 +80,35 @@ const List = ({ children }: React.PropsWithChildren) => {
         };
     }, [tasks, stages]);
 
+    const { mutate: updateTask } = useUpdate<Task, HttpError, TaskUpdateInput>();
+
+    const handleOnDragEnd = (event: DragEndEvent) => {
+        let stageId = event.over?.id as undefined | string | null;
+        const taskId = event.active.id as string;
+        const taskStageId = event.active.data.current?.stageId;
+
+        if (taskStageId === stageId) {
+            return;
+        }
+
+        if (stageId === "unassigned") {
+            stageId = null;
+        }
+
+        updateTask({
+            resource: "tasks",
+            id: taskId,
+            values: {
+                stageId: stageId,
+            },
+            successNotification: false,
+            mutationMode: "optimistic",
+            meta: {
+                gqlMutation: UPDATE_TASK_STAGE_MUTATION,
+            },
+        });
+    };
+
     const handleAddCard = (args: { stageId: string }) => {
         const path =
             args.stageId === "unassigned"
@@ -94,7 +125,7 @@ const List = ({ children }: React.PropsWithChildren) => {
     return (
         <>
             <KanbanBoardContainer>
-                <KanbanBoard>
+                <KanbanBoard onDragEnd={handleOnDragEnd}>
                     <KanbanColumn
                         id={"unassigned"}
                         title={"unassigned"}
